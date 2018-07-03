ed4c076c3215549c1f800f45f24374fa53b4d88c
chrisziegler <chrisrziegler@gmail.com>
2018-07-02 22:34:17 -0700

    added an express graphql server, and refactored methods dealing with composite types having added messages to users

--- index.js
+++ index.js
@@ -1,44 +1,95 @@
-const { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const crypto = require('crypto');
 
 const db = {
   users: [
-    { id: '1', email: 'chrisrziegler@gmail.com', name: 'Chris' },
-    { id: '2', email: 'jim@example.com', name: 'Jim' },
-    { id: '3', email: 'sharon@gmail.com', name: 'Sharon' }
+    {
+      id: '1',
+      email: 'chrisrziegler@gmail.com',
+      name: 'Chris',
+      avatarUrl: 'https://gravatar.com/...'
+    },
+    {
+      id: '2',
+      email: 'jim@example.com',
+      name: 'Jim',
+      avatarUrl: 'https://gravatar.com/...'
+    }
+  ],
+  messages: [
+    { id: '1', userId: '1', body: 'Hello', createdAt: Date.now() },
+    { id: '2', userId: '2', body: 'Hi', createdAt: Date.now() },
+    {
+      id: '3',
+      userId: '1',
+      body: "What's up?",
+      createdAt: Date.now()
+    }
   ]
 };
-// users list itself cant be null
-// atleast an array with no objects [falsy]
-// & User object itself can't be null
+
 const schema = buildSchema(`
   type Query {
     users: [User!]!
+    user(id: ID!) : User
+    messages: [Message!]!
   }
+
+  type Mutation {
+    addUser(email: String!, name: String): User
+  }
+
   type User {
     id: ID!
     email: String!
     name: String
     avatarUrl: String
+    messages: [Message!]!
+  }
+  type Message {
+    id: ID!
+    body: String!
+    createdAt: String
   }
 `);
-// (3) Resolver function
+
+class User {
+  constructor(user) {
+    Object.assign(this, user);
+  }
+  messages() {
+    return db.messages.filter(message => message.userId === this.id);
+  }
+}
+
 const rootValue = {
-  // when we return db users we expect an array
-  // with individual users inside it [line 12]
-  users: () => db.users
+  // wrap each user in a User class, passing in the user
+  users: () => db.users.map(user => new User(user)),
+  user: args => db.users.find(user => user.id === args.id),
+  messages: () => db.messages,
+  // email and name destructured from the the 1st args argument
+  addUser: ({ email, name }) => {
+    const user = {
+      id: crypto.randomBytes(10).toString('hex'),
+      email,
+      name
+    };
+    db.users.push(user);
+    return user;
+  }
 };
 
-graphql(
-  schema,
-  `
-    {
-      users {
-        id
-        email
-      }
-    }
-  `,
-  rootValue
-)
-  .then(res => console.dir(res, { depth: null }))
-  .catch(console.error);
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+  })
+);
+
+app.listen(3000, () => console.log('Listening on 3000'));
