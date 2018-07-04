978e6bf855bc0ff9e082e77578455adc8740e303
chrisziegler <chrisrziegler@gmail.com>
2018-07-03 23:27:37 -0700

    retooled index.js and package.json for Apollo Server

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const { ApolloServer, gql } = require('apollo-server');
 const crypto = require('crypto');
 
 const db = {
@@ -30,10 +28,10 @@ const db = {
   ]
 };
 
-const schema = buildSchema(`
+const typeDefs = gql`
   type Query {
     users: [User!]!
-    user(id: ID!) : User
+    user(id: ID!): User
     messages: [Message!]!
   }
 
@@ -53,43 +51,37 @@ const schema = buildSchema(`
     body: String!
     createdAt: String
   }
-`);
+`;
 
-class User {
-  constructor(user) {
-    Object.assign(this, user);
-  }
-  messages() {
-    return db.messages.filter(message => message.userId === this.id);
-  }
-}
+const resolvers = {
+  Query: {
+    users: () => db.users,
+    user: args => db.users.find(user => user.id === args.id),
+    messages: () => db.messages
+  },
+  Mutation: {
+    addUser: ({ email, name }) => {
+      const user = {
+        id: crypto.randomBytes(10).toString('hex'),
+        email,
+        name
+      };
+      db.users.push(user);
 
-const rootValue = {
-  // wrap each user in a User class, passing in the user
-  users: () => db.users.map(user => new User(user)),
-  user: args => db.users.find(user => user.id === args.id),
-  messages: () => db.messages,
-  // email and name destructured from the the 1st args argument
-  addUser: ({ email, name }) => {
-    const user = {
-      id: crypto.randomBytes(10).toString('hex'),
-      email,
-      name
-    };
-    db.users.push(user);
-    return user;
+      return user;
+    }
+  },
+  User: {
+    // available arguments are (parent, args, context, info) in this case
+    // we need parent to extract (destructure) the user id off the parent
+    // object - The User object that the messages belong to
+    messages: ({ id }) =>
+      db.messages.filter(message => message.userId === this.id)
   }
 };
 
-const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true
-  })
-);
-
-app.listen(3000, () => console.log('Listening on 3000'));
+server.listen().then(serverInfo => {
+  console.log(`Apollo Server listening at ${serverInfo.url}`);
+});
